c6f7f5d41700ea242a2b4f3ddd0ed244d8e69a2f
gwy15 <gwy15thu@gmail.com>
2019-06-11 13:36:54 +0800

    replace manifest.gz instead

--- data/proxy.pac
+++ data/proxy.pac
@@ -1,9 +1,9 @@
 function FindProxyForURL(url, host)
 {
     proxy = "PROXY do.gwy15.com:14514";
-    if (shExpMatch(host, "version.jr.moefantasy.com"))
+    if (shExpMatch(host, "bshot.moefantasy.com"))
         return proxy;
-    if (shExpMatch(host,"version.channel.jr.moefantasy.com"))
+    if (shExpMatch(host,"bshot.moefantasy.com"))
 	    return proxy;
     return "DIRECT";
 }
--- zjsn_uncensor/MITMproxy/addon.py
+++ zjsn_uncensor/MITMproxy/addon.py
@@ -34,23 +34,32 @@ class ZjsnHelper:
     @catch
     def request(self, flow: http.HTTPFlow):
         print('requesting', flow.request.url)
-        if all(domain not in flow.request.host
-               for domain in ('moefantasy.com', 'gwy15.com')):
-            flow.response = http.HTTPResponse.make(404, b'')
+        # if all(domain not in flow.request.host
+        #        for domain in ('moefantasy.com', 'gwy15.com')):
+        #     flow.response = http.HTTPResponse.make(404, b'')
 
     @catch
     def response(self, flow: http.HTTPFlow):
         print(f'response host: {flow.request.host}')
-        if re.match(self.VERSION_HOST, flow.request.host):
-            self.onVersionCheck(flow)
+        if 'warshipgirlsr.manifest.gz' in flow.request.url:
+            print('replacing manifest.gz')
+            with open('data/warshipgirlsr.manifest.gz', 'rb') as f:
+                flow.response.set_content(f.read())
+            return
 
     def onVersionCheck(self, flow: http.HTTPFlow):
         '替换 version check'
         print('replacing version check...')
-        data = json.loads(flow.response.get_text())
+        try:
+            data = json.loads(flow.response.get_text())
+            print(data)
+        except Exception as ex:
+            print(ex)
+            raise
 
         data['ResUrl'] = manifestUrl
         data['ResUrlWu'] = manifestUrl
+        print('data replaced.')
 
         flow.response.set_text(json.dumps(data))
 
