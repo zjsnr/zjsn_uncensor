49490c80b446b4ac4ddbb8ec98b7108b77db655a
gwy15 <gwy15thu@gmail.com>
2019-06-30 21:23:30 +0800

    update

--- zjsn_uncensor/config.py
+++ zjsn_uncensor/config.py
@@ -1,6 +1,6 @@
 MITM_SERVER_PORT = 14514
-STATIC_SERVER_PORT = 14515
-STATIC_URL_PREFIX = f'http://192.168.137.1:{STATIC_SERVER_PORT}/zjsn/'
+STATIC_URL_PREFIX = f'http://static.gwy15.com/moefantasy.com/data/'
+MANIFEST_URL = 'http://static.gwy15.com/moefantasy.com/warshipgirlsr.manifest.gz'
 
 UPLOAD = [
     {
